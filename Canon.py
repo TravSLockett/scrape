e4e4d50bce47796745b79b9ef8f540486057512e
Travis Shen <TravyLockett@gmail.com>
2020-03-29 15:45:37 -0700

    Canon File complete

--- Canon.py
+++ Canon.py
@@ -4,17 +4,19 @@ from lxml import html
 from json import dump, loads
 from requests import get
 from pandas import ExcelWriter
-
+from selenium import webdriver
 
 def parseWalmart():
 
     review_list= []
-
+    driver = webdriver.Safari()
+    driver.get('https://shop.usa.canon.com/shop/en/catalog/eos-rebel-sl3-18-55-black-kit')
     #making the connection to the URL
-    url = 'https://shop.usa.canon.com/shop/en/catalog/eos-rebel-sl3-18-55-black-kit'
-    response = get(url)
-    #download the whole html doc.
-    html_soup = BeautifulSoup(response.text, 'html.parser')
+    #url = 'https://shop.usa.canon.com/shop/en/catalog/eos-rebel-sl3-18-55-black-kit'
+    #response = get(url)
+    response = driver.execute_script("return document.documentElement.outerHTML")
+    driver.quit()
+    html_soup = BeautifulSoup(response, 'html.parser')
 
     #general information, getting the name, price and how many reviews there are
     Name = html_soup.find('span', itemprop = 'name').get_text()
@@ -23,41 +25,40 @@ def parseWalmart():
     #print(ReviewCount)
     #review_url = 'https://www.walmart.com/reviews/product/350931600'
     #response = get(review_url)
-    review_html = BeautifulSoup(response.text, 'html.parser')
+    review_html = BeautifulSoup(response, 'html.parser')
     #get info about the review
-    Reviews = review_html.find_all('li',  itemprop = 'review')
+    Reviews = review_html.find_all('li', itemtype = 'http://schema.org/Review')
     idx = len(Reviews)
     print(idx)
     #print(Reviews)
     count = 0
     for Review in Reviews:
         count = count + 1
-        print(count)
+        #print(count)
         ReviewAuthor = Review.find('span', class_ = 'bv-author').get_text()
-        ReviewTime = Review.find('span', class_ = 'review-footer-submissionTime').get_text()
-        RawReviewStar = Review.find_all('span', class_ = 'star display-inline-block star-rated')
-        ReviewStar = len(RawReviewStar)
-        #print(ReviewStar)
-        raw_review_title = Review.find('h3', class_ ='review-title font-bold')
-        if raw_review_title is None:
-            ReviewTitle = " "
-        else:
-            ReviewTitle = Review.find('h3', class_ ='review-title font-bold').get_text()
+        ReviewTime = Review.find('span', class_ = 'bv-content-datetime-stamp').get_text().strip()
+        ReviewStar = Review.find('span', class_ = 'bv-off-screen').get_text()
 
-        RawReviewContent = Review.find('div', class_ = 'collapsable-content-container')
-        if RawReviewContent is None:
-            ReviewContent = " "
-        else:
-            ReviewContent = Review.find('div', class_='collapsable-content-container').get_text()
 
-        ReviewThumbsUp = Review.find('a', class_ ='width-full review-help-link thumbs-up s-margin-top').get_text()
-        ReviewThumbsDown = Review.find('a', class_ ='width-full review-help-link thumbs-down s-margin-ends').get_text()
-        raw_review_img = Review.find('img', class_ = 'review-media-thumbnail')
+        ReviewTitle = Review.find('h3', class_ ='bv-content-title').get_text()
+
+        ReviewContent = Review.find('div', class_ = 'bv-content-summary-body-text').get_text()
+
+
+        RawReviewThumbsUp = Review.find('button', class_ ='bv-content-btn bv-content-btn-feedback-yes bv-focusable')
+        ReviewThumbsUp = RawReviewThumbsUp.find('span', class_ ='bv-content-btn-count').get_text()
+
+        RawReviewThumbsDown = Review.find('button', class_ ='bv-content-btn bv-content-btn-feedback-no bv-focusable')
+        ReviewThumbsDown = RawReviewThumbsDown.find('span', class_ ='bv-content-btn-count').get_text()
+
+        raw_review_img = Review.find('ul', class_ = 'bv-content-media-container')
         if raw_review_img is None:
-            ReviewImg = " "
+            ReviewImg = "No"
         else:
             #ReviewImg = Review.find('img', class_ = 'review-media-thumbnail').get_text()
-            ReviewImg = raw_review_img['src']
+            ReviewImg = "Yes"
+        print(Name)
+        #print(ReviewAuthor, ReviewTime, ReviewStar, ReviewTitle, ReviewContent, ReviewThumbsUp, ReviewThumbsDown, ReviewImg)
         review_dict = [
             ReviewAuthor, ReviewTime, ReviewStar,ReviewTitle,ReviewContent, ReviewThumbsUp, ReviewThumbsDown, ReviewImg
         ]
@@ -70,19 +71,19 @@ def parseWalmart():
     return data
 
 def writeToExcel(data):
-    df = pd.DataFrame(data[3])
+    df = pd.DataFrame(data[2])
     df.columns = ['Author','Date','Rating','Title','Content','ThumbsUp','ThumbsDown','posted pic?']
-    writer = ExcelWriter(str(data[0])+'_WALMART_3_28_2020'+'.xlsx')
+    writer = ExcelWriter('EOS Rebel SL3 EF-S 18-55mm f4-5.6 IS STM Lens Kit Black'+'_CANON_3_28_2020'+'.xlsx')
     df.to_excel(writer, 'Sheet1', index=False)
     writer.save()
 
 def do():
     extracted_data = parseWalmart()
-    #writeToExcel(extracted_data)
+    writeToExcel(extracted_data)
 
-    f = open('dataCanon.json', 'w')
-    dump(extracted_data, f, indent=4)
-    f.close()
+    #f = open('dataCanon.json', 'w')
+    #dump(extracted_data, f, indent=4)
+    #f.close()
 
 
 if __name__ == '__main__':
