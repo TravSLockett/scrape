06a0eb0536cd82f8bf4d3dbb7e6c0e5579e3ddda
travy <TravyLockett@gmail.com>
2020-04-28 00:54:09 -0700

    Added try except for error checking

--- YoutubeAPI.py
+++ YoutubeAPI.py
@@ -8,12 +8,13 @@ from oauth2client.tools import run_flow
 from oauth2client.file import Storage
 from pandas import ExcelWriter
 import pandas as pd
+from json import dump
 
 def writeToExcel(data):
   df = pd.DataFrame(data[0])
-  df.columns = ['Author', 'Date', 'Rating', 'Title', 'Content', 'ThumbsUp', 'ThumbsDown', 'posted pic?']
+  df.columns = ['posted date', 'title', 'channel', 'channel subs num', 'number of views', 'likes', 'dislikes', 'comment count']
   writer = ExcelWriter(
-    'Beats by Dr. Dre - Solo Pro Wireless Noise Cancelling On-Ear Headphones - Black' + '_BESTBUY_3_31_2020' + '.xlsx')
+    "youtube_4_27_2020_part4" + '.xlsx')
   df.to_excel(writer, 'Sheet1', index=False)
   writer.save()
 
@@ -52,7 +53,7 @@ def get_oauth2_token():
   oauthToken = credentials.access_token
   print(oauthToken)
 
-api_key = "AIzaSyAMnFSuK_tdF6Ra6zDNKqS_MDCwzTRnV5s"
+api_key = "AIzaSyBZLID5dOYB662vVZcDo0euWxIdkJvaqXw"
 
 #test the url
 #video_id = "Z_EltoprrY0"
@@ -68,12 +69,21 @@ class RG:
   def __init__(self):
     pass
   def no_space(self, title: str):
-    title = re.sub('[\W_]+', "_", title)
-    return title.lower()
+    try:
+      title = re.sub('[\W_]+', "_", title)
+      return title.lower()
+    except:
+      return ""
   def get_id(self, url: str):
-    return url.rsplit("=",1)[1]
+    try:
+      return url.rsplit("=",1)[1]
+    except:
+      return ""
   def time(self, timeStamp: str ):
-    return timeStamp.rsplit("T",1)[0]
+    try:
+      return timeStamp.rsplit("T",1)[0]
+    except:
+      return ""
 #getting the data from snippet
 class YouTubeStatsSni:
   def __init__(self, url:str):
@@ -82,18 +92,36 @@ class YouTubeStatsSni:
   def print(self):
     print(self.data)
   def get_video_title(self):
-    return self.data["items"][0]["snippet"]["title"]
+    try:
+      return self.data["items"][0]["snippet"]["title"]
+    except:
+      return ""
   def get_video_des(self):
-    return self.data["items"][0]["snippet"]["description"]
+    try:
+      return self.data["items"][0]["snippet"]["description"]
+    except:
+      return ""
     #return self.data["items"][0]["channelTitle"]
   def download_video(self,youtube_url: str, title: str):
-    YouTube(youtube_url).streams.first().download(filename=title)
+    try:
+      YouTube(youtube_url).streams.first().download(filename=title)
+    except:
+      return str(count)+"is not available"
   def get_date(self):
-    return self.data["items"][0]["snippet"]["publishedAt"]
+    try:
+      return self.data["items"][0]["snippet"]["publishedAt"]
+    except:
+      return ""
   def get_channel(self):
-    return self.data["items"][0]["snippet"]["channelTitle"]
+    try:
+      return self.data["items"][0]["snippet"]["channelTitle"]
+    except:
+      return ""
   def get_channelId(self):
-    return self.data["items"][0]["snippet"]["channelId"]
+    try:
+      return self.data["items"][0]["snippet"]["channelId"]
+    except:
+      return ""
 
 
 #getting the data from the statistics
@@ -104,13 +132,25 @@ class YouTubeStatsSta:
   def print(self):
     print(self.data)
   def get_likes(self):
-    return self.data["items"][0]["statistics"]["likeCount"]
+    try:
+      return self.data["items"][0]["statistics"]["likeCount"]
+    except:
+      return ""
   def get_dislikes(self):
-    return self.data["items"][0]["statistics"]["dislikeCount"]
+    try:
+      return self.data["items"][0]["statistics"]["dislikeCount"]
+    except:
+      return ""
   def get_comment(self):
-    return self.data["items"][0]["statistics"]["commentCount"]
+    try:
+      return self.data["items"][0]["statistics"]["commentCount"]
+    except:
+      return ""
   def get_views(self):
-    return self.data["items"][0]["statistics"]["viewCount"]
+    try:
+      return self.data["items"][0]["statistics"]["viewCount"]
+    except:
+      return ""
 
 class YouTubeCha:
   def __init__(self, url: str):
@@ -119,63 +159,76 @@ class YouTubeCha:
   def print(self):
     print(self.data)
   def get_subs(self):
-    return self.data["items"][0]["statistics"]["subscriberCount"]
+    try:
+      return self.data["items"][0]["statistics"]["subscriberCount"]
+    except:
+      return ""
 
-link_file = "something.csv"
+link_file = "something2.csv"
 with open(link_file, "r") as f:
   content = f.readlines()
 
 content = list(map(lambda s: s.strip(), content))
 #content = list (map(lambda s: s.strip(','), content))
-
+count = 219
 regular = RG()
+data_list = []
 for url in content:
   video_id = regular.get_id(url)
   urlSni = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={video_id}&key={api_key}"
-  urlSta = f"https://www.googleapis.com/youtube/v3/videos?part=statistics&id={video_id}&key={api_key}"
+  #urlSta = f"https://www.googleapis.com/youtube/v3/videos?part=statistics&id={video_id}&key={api_key}"
   #get channel ID
-  yt_statsChaID = YouTubeStatsSni(urlSni)
-  channelID = yt_statsChaID.get_channelId()
+  #yt_statsChaID = YouTubeStatsSni(urlSni)
+  #channelID = yt_statsChaID.get_channelId()
 
-  urlCha = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channelID}&key={api_key}"
-  yt_statsSubs = YouTubeCha(urlCha)
-  channelSubs = yt_statsSubs.get_subs()
+  #urlCha = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channelID}&key={api_key}"
+  #yt_statsSubs = YouTubeCha(urlCha)
+  #channelSubs = yt_statsSubs.get_subs()
 
   yt_statsSni = YouTubeStatsSni(urlSni)
-  yt_statsSta = YouTubeStatsSta(urlSta)
+  #yt_statsSta = YouTubeStatsSta(urlSta)
 
   #get title
   title = yt_statsSni.get_video_title()
   title = regular.no_space(title)
   #download video
-  #yt_statsSni.download_video(url,title)
+  yt_statsSni.download_video(url,str(count) + "_"+ title)
   #get the date of publication
-  date = yt_statsSni.get_date()
-  date = regular.time(date)
+  #date = yt_statsSni.get_date()
+  #date = regular.time(date)
   #get description
-  description = yt_statsSni.get_video_des()
+  #description = yt_statsSni.get_video_des()
   #get channel name
-  channel = yt_statsSni.get_channel()
+  #channel = yt_statsSni.get_channel()
   #get likes and dislikes
-  likes = yt_statsSta.get_likes()
-  dislikes = yt_statsSta.get_dislikes()
+  #likes = yt_statsSta.get_likes()
+  #dislikes = yt_statsSta.get_dislikes()
   #get number of comments
-  commentCount = yt_statsSta.get_comment()
+  #commentCount = yt_statsSta.get_comment()
   #get number of views
-  views = yt_statsSta.get_views()
+  #views = yt_statsSta.get_views()
+  #f = open(str(count)+title+".txt", "w")
+  #dump(description, f, indent=4)
+  #f.close()
+  #data_dict = [date, title, channel, channelSubs, views, likes,dislikes, commentCount ]
+  #data_list.append(data_dict)
+  print(count)
+  count += 1
+  #print("title:",title)
+  #print("date:",date)
+  #print("description: ",description)
+  #print("Likes: ",likes)
+  #print("Dislikes:",dislikes)
+  #print("Num of Comments: ", commentCount)
+  #print("Num of views:",views)
+  #print("Channel: ",channel)
+  #print("channel subs: ", channelSubs)
 
-
-  print("title:",title)
-  print("date:",date)
-  print("description: ",description)
-  print("Likes: ",likes)
-  print("Dislikes:",dislikes)
-  print("Num of Comments: ", commentCount)
-  print("Num of views:",views)
-  print("Channel: ",channel)
-  print("channel subs: ", channelSubs)
-
-
+#data = [data_list]
+#writeToExcel(data)
+#f = open('YoutubeTest.json', 'w')
+#dump(data, f, indent=4)
+#f.close()
 
 #get_oauth2_token()
 
